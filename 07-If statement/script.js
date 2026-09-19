c27064a64c12f98e67f219ca711f1cb41f8714c6
tfabien1 <turatsinzefabien1@gmail.com>
2026-09-20 00:10:31 +0200

    Add files via upload

---
+++ 07-If statement/script.js
@@ -0,0 +1,34 @@
+ //if statement:these they are statements that help us to compare different contracepting statements
+let x = 3;
+let y = 10;
+let z = x + y ;
+
+if(z < 10){ 
+ z = "It is the true value"}
+
+else{z =  "It is not the true value"
+};
+console.log(z);
+
+const myText = document.getElementById("myText");
+const mysubmit = document.getElementById("mysubmit");
+const myresults = document.getElementById("myresults");
+let age;
+mysubmit.onclick = function(){
+    age =myText.value;
+    age = Number(age);
+    
+    
+ if(age >=50){
+    myresults.textContent = "you are too old for the event";
+ }
+ else if(age <= 50){
+    myresults.textContent = "you allowed to attend the event";
+ }
+ if(age == 17){
+
+    myresults.textContent = "To attend the event you should be 18+"; 
+ }
+ else if(age <= 0 ){ myresults.textContent =  " Age can not go below zero";
+ }
+}
