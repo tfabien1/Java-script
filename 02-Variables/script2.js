0583b95c8de857350352f0123288fdcecb8e2dcf
tfabien1 <turatsinzefabien1@gmail.com>
2026-09-19 19:59:59 +0200

    Add files via upload

---
+++ 02-Variables/script2.js
@@ -0,0 +1,28 @@
+ //variables in javascript
+    //1.variabe decaration let x;
+    //2.variable assignment x = 100;
+
+    let age = 100;
+    console.log(typeof age);
+   console.log(age);
+   let price = 100;
+   console.log(typeof price);
+   console.log("the price is ${price}");
+
+    //boolleans
+   //they are either true or false
+
+   let isStudent = false;
+   console.log(typeof isStudent);
+   let save = true;
+   console.log('is the sex a boy ${save}');
+   let enroll = false;
+   console.log(enroll);
+   console.log(typeof enroll);
+   let girl = true;
+   console.log(girl);
+
+   String
+
+   let Name = "keza";
+   console.log(typeof Name);
---
+++ 03-Operators/script3.js
@@ -0,0 +1,46 @@
+//operators
+
+let Bob = 30;
+Bob = Bob +=1;
+Bob = Bob -=2;
+Bob = Bob *2;
+Bob = Bob %=2;
+Bob++;
+Bob--;
+console.log(Bob);
+console.log(typeof Bob);
+
+
+let boy;
+boy = window.prompt("what is the ID");
+console.log(boy);
+
+let username;
+document.getElementById(pic).onclick = function(){
+    username = document.getElementById(hip).value;
+    console.log(username);
+};
+
+//predence operators
+//1.paranthesis
+//2.exponent
+//3.multiplication and division
+//4.addition and subtraction
+
+
+
+let result = 6*4/16+11;
+let results = 12/90 %12;
+console.log(result);
+console.log(results);
+
+
+
+//using operators with  values//
+
+let student = 25;
+student = student + 12;
+student = student - 12;
+student = student / 13;
+student = student % 1;
+console.log(student);
