b8ac5102c098653ce372128d6cba8c963d85af76
tfabien1 <turatsinzefabien1@gmail.com>
2026-09-19 22:22:56 +0200

    Add files via upload

---
+++ 06-Counter/script.js
@@ -0,0 +1,29 @@
+//A counter 
+const decreaseBtn = document.getElementById("decreaseBtn" );
+const resetBtn = document.getElementById("resetBtn");
+const increaseBtn = document.getElementById("increaseBtn" );
+const countlabel = document.getElementById("countlabel");
+let count = 0;
+
+decreaseBtn.onclick = function(){
+    count--;
+    countlabel.textContent = count;
+
+
+
+}
+resetBtn.onclick = function(){
+    count = 0;
+    countlabel.textContent = count;
+
+
+
+}
+
+increaseBtn.onclick = function(){
+    count++;
+    countlabel.textContent = count;
+
+
+
+}
