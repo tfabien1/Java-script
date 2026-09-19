86834914dbdb9f1d1c6138a15855fe072ab8eedc
tfabien1 <turatsinzefabien1@gmail.com>
2026-09-20 01:16:03 +0200

    Remove duplicate alert and console.log statements

--- Basics of js/script.js
+++ Basics of js/script.js
@@ -1,7 +1,2 @@
-//ways of displaying the out put 
-window.alert("hello is this you page");
-window.alert("thank you to visit our page");
-console.log("Hello what is the next step");
-console.log("if it is all about rwanda show off");
-//we can also make the output using the id 
+
 document.getElementById("myparagraph").textContent="Hope everyone is concerned";
