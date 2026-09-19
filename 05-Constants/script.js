2ba93897f9dcb8914bbe7dd56bba739073ce2876
tfabien1 <turatsinzefabien1@gmail.com>
2026-09-19 21:36:54 +0200

    Add files via upload

---
+++ 05-Constants/script.js
@@ -0,0 +1,8 @@
+//const= values that does not change
+let pi = 3.1456;
+let rad;
+let circumference;
+rad = window.prompt("What is the radiaus to find the circumfrence ?");
+rad = Number(rad);
+circumference = 2 * pi * rad;
+console.log(circumference);
