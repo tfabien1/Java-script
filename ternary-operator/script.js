a6455b3f469be4ce1fe9a4c7c9cb0848febd38c3
tfabien1 <turatsinzefabien1@gmail.com>
2026-09-20 20:27:37 +0200

    Add files via upload

---
+++ ternary-operator/script.js
@@ -0,0 +1,8 @@
+//ternary operator = a shortcut to if{}statements hel to assign a variable 
+//based on a condition ? codeIfTrue :codeIfFalse;
+let age = 1;
+let message = age >= 18 ? "you are an adult" : "you are a minor";
+console.log(message);
+
+
+
