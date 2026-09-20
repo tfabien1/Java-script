8451cfc7392958a68fd9b5d3ebcc2bf332269269
tfabien1 <turatsinzefabien1@gmail.com>
2026-09-20 20:50:10 +0200

    Add files via upload

---
+++ Swiches/script.js
@@ -0,0 +1,24 @@
+//switche=can be an efficient replacement to many else if statement
+let day = 3;
+switch(day){
+     
+    case 1 :
+        console.log("Its Monday");
+        break;
+        case 2  :
+        console.log("Its Tuesdayday");
+        break;
+        case 3 :
+        console.log("Its Wednesday");
+        break;
+        case 4 :
+        console.log("Its thursday");
+        break;case 5 :
+        console.log("Its friday");
+        break;
+
+        default:
+        console.log("${day} is not a day");
+
+
+}
