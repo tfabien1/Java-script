7b01702bcc0239e770807992786588f5860d7a86
tfabien1 <turatsinzefabien1@gmail.com>
2026-09-21 23:09:27 +0200

    Add files via upload

---
+++ Logical operators/script.js
@@ -0,0 +1,17 @@
+//const temp =20;
+
+//if(temp > 0 && temp <= 21){
+  //  console.log("the weather is good");
+//}
+//else{
+   // console.log("the weather is Bad");
+//}
+const issunny = false;
+if(!issunny){
+    console.log("it is cloude")
+
+}
+else{
+    console.log("it is sunny")
+}
+ 
