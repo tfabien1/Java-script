9ef35538642bbf5c33f69998d3bc19fcd545251a
tfabien1 <turatsinzefabien1@gmail.com>
2026-09-24 19:43:31 +0200

    Add files via upload

---
+++ Functions/script.js
@@ -0,0 +1,11 @@
+function happybirthday(username,age){
+    console.log("Happy birthday to you!");
+     console.log("Happy birthday to you!");
+      console.log(`Happy birthday dear ${username}!`);
+       console.log("Happy birthday to you!");
+        console.log("Happy birthday to you!");
+         console.log(`you are ${age} years old `);
+}
+happybirthday("keza",25);
+    
+   
