79162768d2335a9299f9f3562ab5fb5c1f8845e2
tfabien1 <turatsinzefabien1@gmail.com>
2026-09-24 19:46:50 +0200

    Add files via upload

---
+++ Spread operator/script.js
@@ -0,0 +1,10 @@
+//let numbers = [1,2,3,4,5];
+//let maximum = Math.max(...numbers) ;
+//let minimum = Math.min(...numbers);
+//console.log(minimum);
+//console.log(maximum);
+
+let username = "Frank";
+let letters = [...username].join("-");
+console.log(letters);
+
