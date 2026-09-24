feb7ce5d4e889d2cf935f077a0176ff936b11cf7
tfabien1 <turatsinzefabien1@gmail.com>
2026-09-24 19:40:33 +0200

    Add files via upload

---
+++ Array/script.js
@@ -0,0 +1,13 @@
+let fruits = ["apple","orange","banana"];
+//fruits.push("coconut");
+//fruits.pop("coconut");
+//fruits.unshift("mango");
+//fruits.shift();
+
+//let numOfFruits = fruits.length;
+//let index = fruits.indexOf("banana");
+//console.log(numOfFruits);
+//console.log(fruits[1]);
+//console.log(index);
+
+
